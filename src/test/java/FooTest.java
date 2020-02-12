97b7e9fa32fae98f859225b800237edf27d95d90
Sharda Arbale <sharda.darekar@gmail.com>
2020-02-12 13:21:15 +0100

    Changed foo and Foo test

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -3,6 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class FooTest {
     @org.junit.jupiter.api.Test
     void myMethod() {
-        assertEquals(false, Foo.myMethod());
+        assertEquals(true, Foo.myMethod());
     }
 }
